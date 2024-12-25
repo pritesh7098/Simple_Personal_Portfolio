9da26e04a11a9a908d8dde7f4d401da03e32e7b0
hey_pritesh <priteshdhanad2001@gmail.com>
2024-12-25 17:13:05 +0530

    Updated main page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,37 +1,60 @@
-'use client'
+"use client";
 import Avatar from "./components/Avatar";
 import ProjectCardList from "../projects/components/ProjectCardList";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Education from "@/components/Education";
 import { RainbowButton } from "@/components/ui/rainbow-button";
-import { Link as ScrollLink } from 'react-scroll';
+import { Link as ScrollLink } from "react-scroll";
 import Coursework from "@/components/Coursework";
-import { inter, bricolage_grotesque } from '@/utils/fonts';
+import { inter, bricolage_grotesque } from "@/utils/fonts";
 import Contact from "@/components/Contact";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { GithubGraph } from "@/components/ui/github";
 import Title from "@/components/ui/Title";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }} className="max-[350px]:overflow-hidden mt-8 max-sm:mt-0">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-[350px]:overflow-hidden mt-8 max-sm:mt-0"
+    >
       <div className="w-full flex justify-center py-5 pt-36 dark:bg-black">
         <div className="w-2/3 max-sm:w-full flex flex-col items-center">
           <div>
             <Avatar />
           </div>
           <div className="mt-4 px-32 max-sm:px-4">
+            <h1
+              className={`!text-[3rem] mt-2 max-sm:!text-[1.6rem] !whitespace-nowrap font-bold tracking-tight text-center ${bricolage_grotesque}`}
+            >
+              Hi, I&apos;m Pritesh Dhanad👋
+            </h1>
 
-            <h1 className={`!text-[3rem] mt-2 max-sm:!text-[1.6rem] !whitespace-nowrap font-bold tracking-tight text-center ${bricolage_grotesque}`}>Hi, I&apos;m Pritesh Dhanad👋</h1>
-
-            <h1 className={`mt-2 max-sm:mt-5 max-sm:px-3 !text-base max-sm:!text-sm text-center !font-normal !tracking-normal inter !leading-6 ${inter}`}>Full Stack Web Developer Building Impactful Web Applications. I specialize in creating production-ready web apps from the ground up, with a focus on both frontend and backend development. Passionate about solving real-world problems and always open to new challenges.</h1>
-
+            <h1
+              className={`mt-2 max-sm:mt-5 max-sm:px-3 !text-base max-sm:!text-sm text-center !font-normal !tracking-normal inter !leading-6 ${inter}`}
+            >
+              "Passionate Software Developer with a proven ability to solve
+              complex problems through elegant and efficient code. I specialize
+              in building high-quality, scalable applications across diverse
+              platforms using cutting-edge technologies."
+            </h1>
           </div>
           <div className="mt-10">
             <RainbowButton>
-              <ScrollLink to="contact-section" activeClass="active" smooth={true} offset={-120} duration={1100}>Get in touch</ScrollLink>
+              <ScrollLink
+                to="contact-section"
+                activeClass="active"
+                smooth={true}
+                offset={-120}
+                duration={1100}
+              >
+                Get in touch
+              </ScrollLink>
             </RainbowButton>
           </div>
         </div>
@@ -43,11 +66,17 @@ export default function Home() {
 
       <div className="w-full flex flex-col items-center mt-8 mb-4 gap-4 max-sm:hidden">
         <Title title="GitHub Contributions" />
-        <Link href={'https://github.com/Fardeen26'} target="_blank">
+        <Link href={"https://github.com/pritesh7098"} target="_blank">
           <GithubGraph
-            username="Fardeen26"
+            username="pritesh7098"
             blockMargin={2}
-            colorPallete={["#1e1e2f", "#5a3e7a", "#7e5aa2", "#a87cc3", "#d9a9e6"]}
+            colorPallete={[
+              "#1e1e2f",
+              "#5a3e7a",
+              "#7e5aa2",
+              "#a87cc3",
+              "#d9a9e6",
+            ]}
           />
         </Link>
       </div>
@@ -57,11 +86,32 @@ export default function Home() {
       </div>
 
       <div className="w-full flex justify-center mt-4">
-        <Experience company_link={"https://x.com/fardeen14693425"} company_logo={"/stealth-startup.jpeg"} company_name={"Stealth Startup"} duration={"Feb 2024 - June 2024"} job_title={"Teaching Assistant (MERN Stack)"} description={"Mentored over 5,000 students in MERN Stack development, resolving 500+ coding issues and improving understanding. Achieved an average rating of 4.75 for effective teaching and support."} />
+        <Experience
+          company_link={"https://x.com/fardeen14693425"}
+          company_logo={"/Academor.jpg"}
+          company_name={"ACADEMOR EDTECH PRIVATE LIMITED"}
+          duration={"Jan 2024 - Mar 2024"}
+          job_title={"Frontend Developer Intern"}
+          description={
+            "Gained comprehensive frontend development experience through internship focused on UI development and user experience optimization. Mastered the art of pixel-perfect implementation while building responsive, cross-platform web applications. Enhanced problem-solving abilities through real-world project challenges and collaborative development environments."
+          }
+        />
       </div>
 
       <div className="w-full flex justify-center mt-4">
-        <Education institute_link={"https://www.uok.ac.in/"} institute_logo={"/uok.gif"} course_title={"Bachelors of Computer Application"} ending_date={"Expected July 2025"} institute_name={"University of Kota"} cgpa={8.5} description={"Currently pursuing a BCA degree, focusing on software development, web technologies, and programming languages. Maintaining a CGPA of 8.5, with coursework in data structures, operating systems, DBMS, computer networks."} />
+        <Education
+          institute_link={"https://www.kkwagh.edu.in/engineering/"}
+          institute_logo={"/kk.png"}
+          course_title={"Master's In Computer Applications"}
+          ending_date={" Nov-2022 - June 2024"}
+          institute_name={
+            "K.K. Wagh Institute of Engineering Education and Research."
+          }
+          location={"Nashik, Maharashtra"}
+          description={
+            "Recent Master's in Computer Applications graduate with Grade A distinction, specializing in Software Development. Academic coursework concentrated on diverse programming languages, web technologies, and software development methodologies. Strong foundation in both theoretical concepts and practical applications of modern development tools and frameworks."
+          }
+        />
       </div>
 
       <div className="w-full flex justify-center mt-4">
