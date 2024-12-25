d794005115241006dfadfe9b4e242182e9d8d944
hey_pritesh <priteshdhanad2001@gmail.com>
2024-12-25 17:12:16 +0530

    Updated components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,20 @@
-import { Link } from '@radix-ui/themes'
-import React from 'react'
-
+import { Link } from "@radix-ui/themes";
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='w-full px-[400px] max-[1285px]:px-[350px] max-lg:px-64 max-sm:px-4 py-10 flex flex-col max-sm:text-center items-center mt-4 pb-8'>
+    <footer className="w-full px-[400px] max-[1285px]:px-[350px] max-lg:px-64 max-sm:px-4 py-10 flex flex-col max-sm:text-center items-center mt-4 pb-8">
       <div className="w-full">
-        <p className='text-sm max-sm:text-xs'>Designed and Developed by <Link href='https://x.com/intent/follow?screen_name=fardeen14693425' target='_blank' >Fardeen Mansoori</Link></p>
+        <p className="text-sm max-sm:text-xs">
+          {" "}
+          Developed by{" "}
+          <Link href="https://x.com/pritesh_7098" target="_blank">
+            Pritesh Dhanad{" "}
+          </Link>
+        </p>
       </div>
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
